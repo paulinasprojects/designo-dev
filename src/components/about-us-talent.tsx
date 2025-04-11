94f00bee94b94ecb1f75e0ced027255192f97046
paulinasprojects <virtualpaula93@gmail.com>
2025-04-11 10:51:48 +0300

    added responsive about us talent section

---
+++ src/components/about-us-talent.tsx
@@ -0,0 +1,22 @@
+import "../styles/about-us-talent.css";
+
+export const AboutUsTalent = () => {
+  return (
+    <section className="about-us-talent-section">
+      <div className="about-us-talent-main-container">
+      <div className="about-us-talent-image-container">
+          <img src="/assets/about/desktop/image-world-class-talent.jpg" alt="" />
+          <img src="/assets/about/tablet/image-world-class-talent.jpg" alt="" className="talent-tablet-image" />
+          <img src="/assets/about/mobile/image-world-class-talent.jpg" alt="" className="talent-mobile-image" />
+        </div>
+        <div className="about-us-talent-title-container">
+          <h1>World class talent</h1>
+          <p>
+            We are a crew of strategists, problem-solvers, and technologists. Every design is thoughtfully crafted from concept to launch, ensuring success in its given market. We are constantly updating our skills in a myriad of platforms. 
+          </p>
+          <p>Our team is multi-disciplinary and we are not merely interested in form — content and meaning are just as important. We give great importance to craftsmanship, service, and prompt delivery. Clients have always been impressed with our high-quality outcomes that encapsulates their brand’s story and mission.</p>
+        </div>
+      </div>
+    </section>
+  )
+}
--- src/pages/about-us-page.tsx
+++ src/pages/about-us-page.tsx
@@ -1,9 +1,11 @@
 import { AboutUsHero } from "../components/about-us-hero"
+import { AboutUsTalent } from "../components/about-us-talent"
 
 const AboutUsPage = () => {
   return (
     <div>
       <AboutUsHero/>
+      <AboutUsTalent/>
     </div>
   )
 }
